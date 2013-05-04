540d805d20ca7c3507775a79d44d3a76d55bb127
vaibhav puri <vaibhav@taggito.com>
2013-05-04 20:20:46 +0200

    top up functionality added

--- assets/www/nfc.js
+++ assets/www/nfc.js
@@ -1,31 +1,47 @@
-function writeTag(nfcEvent) {
 
 
-        var type = "text/plain",
-        id = 123,
-        payload = "1"
-        ndefRecord = ndef.record(ndef.TNF_MIME_MEDIA, type, id, payload);
-		ndefMessage = [];
-        ndefMessage.push(ndefRecord);
-        nfc.write(ndefMessage);
+function write_payload(nfcEvent){
+	var type = "text/plain";
+    id = 123;
 
-		} 
+	payload =  getURLParameter("topup_value");
+    ndefRecord = ndef.record(ndef.TNF_MIME_MEDIA, type, id, payload);
+	ndefMessage = [];
+    ndefMessage.push(ndefRecord)
+    nfc.write(ndefMessage,successTagWrite, failedTagWrite );
+  //
+     navigator.notification.alert("Credit Added: " + payload );
+}
+
+function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
 
 
-function ndefread(nfcEvent){
-		var appid= document.getElementById("appInfo")
-      appid.innerHTML = (JSON.stringify(nfcEvent.tag.ndefMessage));
+
+
+function ndef_read_callback(nfcEvent){
+	var appid= document.getElementById("topup_value")
+    appid.innerHTML = (JSON.stringify(nfcEvent.tag.ndefMessage));
 }	
 
 
 function successTagWrite() {
-                console.log("Wrote Tag Successfully!");}        
+	navigator.notification.vibrate(100);
+    navigator.notification.alert("Credit Added: " + payload );
+
+}        
 
 function failedTagWrite() {
-                console.log("Failed Tag Write!");}        
+	navigator.notification.alert("Failed Tag Write!");
+}        
 
 function successTagRead() {
-                console.log("Read Tag Successfully!");}        
+console.log("Read Tag Successfully!");
+}        
 
-function failedTagWrite() {
-                console.log("Failed Tag Read!");}        
+function failedTagRead() {
+console.log("Failed Tag Read!");
+}        
