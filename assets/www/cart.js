bdb55f335b175bbd3e73f901e87d912540c3c753
vaibhav puri <vaibhav@taggito.com>
2013-05-08 16:41:25 +0200

    cart functionality v1 added

--- assets/www/cart.js
+++ assets/www/cart.js
@@ -1,32 +1,79 @@
-window.onload = function ()
- {
-   
+cartValue = 0;
+
+function debit_tag(credit){
+	creditAvail = parseFloat(credit);
+	if (creditAvail<cartValue)
+		{
+			navigator.notification.alert("Not enough balance, please top-up");
+		}
+		else
+		{
+			newCredit = creditAvail - cartValue;
+			payload = newCredit.toString();
+			writeTag(payload);
+			navigator.notification.alert("Amount Debited: "+ cartValue + "\n" + "Initial Credit: "+creditAvail+"\n"+"Total Available Credit: "+newCredit);		
+		}
+
+
+}
+
+
+function current_credit(nfcEvent){
+	try{
+		tag_payload = nfcEvent.tag.ndefMessage[0]['payload'];
+		credit = nfcTagStringTasks(tag_payload);
+		debit_tag(credit);
+		}
+	catch(e)
+		{
+		navigator.notification.alert("Error Found" + e);
+		}		
+}
+
+
+
+function onDeviceReady(){
+	pay.onclick = function(){nfc.addNdefListener(current_credit,successTagRead,failedTagRead);}
+}
+
+
+
+
+function add_to_cart(increment){  	
+  cartValue = cartValue +parseFloat(increment);
+  cartBox = document.getElementById('cartValue');
+  cartBox.innerHTML = "Total= " + cartValue.toString();
+}
+
+
+function initialiseIds()
+{
+
 	beers = document.getElementById('beers');
 	pizza = document.getElementById('pizza');
 	burger = document.getElementById('burger');
 	pay= document.getElementById('pay')
 	
+	}
+	
+function registerEvents() {
+	
 	beers.onclick = function(){add_to_cart(beers.getAttribute('data-value'));}
 	pizza.onclick = function(){add_to_cart(pizza.getAttribute('data-value'));}
 	burger.onclick = function(){add_to_cart(burger.getAttribute('data-value'));}
 	sausage.onclick = function(){add_to_cart(sausage.getAttribute('data-value'));}
-
-	pay.onclick = function(){debit_tag();}
-
-
-	var imported = document.createElement('script');
-	imported.src = 'phonegap-nfc.js';
-	document.head.appendChild(imported);
-
-	
+	document.addEventListener("deviceready", onDeviceReady, true);
 
 
 }
 
 
 
-
-function add_to_cart(increment){
-  total = total +parseFloat(increment);
+window.onload = function ()
+ {
+   
+	initialiseIds();
+	registerEvents();
 }
+
 
--- assets/www/nfc.js
+++ assets/www/nfc.js
@@ -32,4 +32,54 @@ function failedTagRead() {
 console.log("Failed Tag Read!");
 }        
 
+function parse_credit_string(stringToParse)
+{
+	parsed_string = stringToParse.substring(3,stringToParse.length);
+  	return parsed_string;
+}
 
+function error_handle_string(test_value)
+{
+  
+	if (isNaN(parseFloat(test_value))||test_value==="")
+  		{
+    		test_value ="0";
+    		navigator.notification.alert("No credit was added - input was incorrect ");  
+  		}
+	return test_value;
+}
+
+
+function disable_event_button(name){
+	theButton = document.getElementsByName(name)[0];
+	theButton.disabled = true;
+	
+}
+
+
+function enable_event_button(name){
+	theButton = document.getElementsByName(name)[0];
+	theButton.disabled = false;
+	
+}
+
+function nfcTagStringTasks(tempValue){
+	tempCredit = nfc.bytesToString(tempValue);
+  
+  	tempCredit = parse_credit_string(tempCredit);
+    
+  	tempCredit = error_handle_string(tempCredit);  
+  	
+  	return tempCredit; //always move variables around as strings
+
+}
+
+
+function writeTag(payload)
+{
+
+	ndefRecord = ndef.textRecord(payload);
+  	ndefMessage = [];
+    ndefMessage.push(ndefRecord);
+    nfc.write(ndefMessage,successTagWrite, failedTagWrite );
+}
